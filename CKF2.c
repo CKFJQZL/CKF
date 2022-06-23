5724ec996d651c72424c48257a70e463772dea54
CKFJQZL <2665671984@qq.com>
2022-06-23 20:57:07 +0800

    添加项目文件。

---
+++ CKF2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, j;
+	int a[4][4];
+	for (i = 0;i < 4;i++)
+	{
+		for (j = 0;j < 4;j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	int sum = 0;
+	for (i = 0;i < 4;i++)
+	{
+		for (j = 0;j < 4;j++)
+		{
+			if (i == j)
+			{
+				sum = sum + a[i][j];
+				break;
+			}
+		}
+	}
+	printf("%d", sum);
+	return 0;
+}
