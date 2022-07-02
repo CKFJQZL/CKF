4dd73ed794a894ef6035a876f33b2a9628b4f47e
CKFJQZL <2665671984@qq.com>
2022-07-02 22:03:22 +0800

    c-code

--- CKF2.c
+++ CKF2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 /*int main()
+* //输出一个矩阵的主对角线之和。
 {//ckf
 	int i, j;
 	int a[4][4];
@@ -48,4 +49,24 @@
 	return 0;
 }*/
 //3,已知某商品单价为30，数量为x。求商品的总价钱。用宏定义物品单价。
+/*int main()
+{
+	int x = 0, c = 0;
+	//scanf("%d", &x);
+	c = x * 30;
+	printf("%d", c);
+	return 0;
+}*/
+//4,输入一个三位数，输出该数字的百位，十位，个位，数字之间用一个空格隔开。
+int main()
+{
+	int a, b, c, d, e;
+	scanf("%d", &a);
+	b = a / 100;
+	c = a % 100;
+	d = c / 10;
+	e = c % 10;
+	printf("%d %d %d", b, d, e);
+	return 0;
+}
 
